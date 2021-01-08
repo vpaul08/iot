7b5c1fd5b3a815df9ccedad1715bcdbf7b81e5a6
rishinder.rana <rishinder.rana@vidyard.com>
2021-01-08 18:27:17 -0500

    Init commit for UI

---
+++ openhab-ui/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+app.set('views', './views')
+app.set('view engine', 'jade')
+
+app.get('/360vr', (req, res) => {
+  res.render('360vr');
+})
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
